5da5a1540e4e28421482932243e887272c31ba22
CVan <cvandesande@opendmz.com>
2026-05-27 09:04:25 +0100

    build: support OpenWrt builds in NixOS shell
    Use an FHS shell for OpenWrt host tooling on NixOS and carry the environment fixes validated by the local full build: disable Nix hardening wrappers, prefer GCC binutils wrappers for LTO archives, expose libcrypt, and make fakeroot skip real host chown attempts.
    
    Enable the developer/ccache seed options used by the local and nightly rebuild flow.

--- shell.nix
+++ shell.nix
@@ -1,42 +1,79 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}
+, extraPkgs ? []
+}:
 
-pkgs.mkShell {
-  name = "openwrt-dev";
+let
+  fixWrapper = pkgs.runCommand "fix-wrapper" {} ''
+    mkdir -p $out/bin
+    for i in ${pkgs.gcc.cc}/bin/*-gnu-gcc*; do
+      ln -s ${pkgs.gcc}/bin/gcc $out/bin/$(basename "$i")
+    done
+    for i in ${pkgs.gcc.cc}/bin/*-gnu-{g++,c++}*; do
+      ln -s ${pkgs.gcc}/bin/g++ $out/bin/$(basename "$i")
+    done
+    ln -sf ${pkgs.gcc.cc}/bin/{,*-gnu-}gcc-{ar,nm,ranlib} $out/bin
+  '';
 
-  hardeningDisable = [ "format" ];
-
-  packages = with pkgs; [
-    bash
-    bc
-    binutils
-    bzip2
-    coreutils
-    diffutils
-    file
-    findutils
-    flex
-    gawk
-    gcc
-    gettext
-    git
-    gnugrep
-    gnumake
-    gnused
-    gnutar
-    gzip
-    ncurses
-    openssl
-    patch
-    perl
-    pkg-config
-    python3
-    rsync
-    unzip
-    util-linux
-    wget
-    which
-    xz
-    zlib
-    zstd
-  ];
+  fhs = pkgs.buildFHSEnv {
+    name = "openwrt-env";
+    runScript = "bash";
+    targetPkgs = pkgs: with pkgs; [
+      bash
+      bc
+      bison
+      bzip2
+      ccache
+      coreutils
+      diffutils
+      file
+      findutils
+      fixWrapper
+      flex
+      gawk
+      gcc
+      gettext
+      git
+      glibc.static
+      gnumake
+      gnugrep
+      gnused
+      gnutar
+      gzip
+      ncurses
+      openssl
+      patch
+      perl
+      pkg-config
+      (python3.withPackages (ps: [ ps.setuptools ]))
+      rsync
+      subversion
+      swig
+      unzip
+      util-linux
+      wget
+      which
+      xz
+      libxcrypt
+      zlib
+      zlib.static
+      zstd
+    ] ++ extraPkgs;
+    multiPkgs = null;
+    extraOutputsToInstall = [ "dev" ];
+    profile = ''
+      export hardeningDisable=all
+      export NIX_HARDENING_ENABLE=
+      export NIX_HARDENING_ENABLE_x86_64_unknown_linux_gnu=
+      export AR=gcc-ar
+      export NM=gcc-nm
+      export RANLIB=gcc-ranlib
+      export FAKEROOTDONTTRYCHOWN=1
+    '';
+  };
+in pkgs.mkShell {
+  name = "openwrt-env-wrapper";
+  packages = [ fhs ];
+  shellHook = ''
+    exec ${fhs}/bin/openwrt-env
+  '';
 }
